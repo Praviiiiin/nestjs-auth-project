f0df5fb9aef08d355f4c78f4afce806417284c4e
Pravin <epravin2005@gmail.com>
2026-05-20 00:34:12 +0530

    add AuthModule to AppModule and create AuthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-auth'),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
 }) 
 export class AppModule {}
