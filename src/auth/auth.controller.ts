29b0451751c67a2c7fa49df0265bcc9788dd3259
Pravin <epravin2005@gmail.com>
2026-05-20 18:17:21 +0530

    implement AuthController with registration endpoint and integrate AuthService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post  } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    register(@Body() body: any) {
+        return this.authService.register(body);
+    }
+}
+
+
