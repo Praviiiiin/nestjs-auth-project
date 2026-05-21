c67c8774e0a75453f4c9d625752bb41b0b31a115
Pravin <epravin2005@gmail.com>
2026-05-22 01:15:19 +0530

    refactor: update AuthController to use specific DTOs for register and login methods feat: add global validation pipe in main.ts for request validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,26 @@
 import { Controller, Body, Post, Get, Req, UseGuards  } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth-guard';
-
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('register')
-    register(@Body() body: any) {
+    register(@Body() body: RegisterDto) {
         return this.authService.register(body);
     }
 
     @Post('login')
-    login(@Body() body: any) {
+    login(@Body() body: LoginDto) {
         return this.authService.login(body);
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    getProfile(@Req() req:any) {
+    getProfile(@Req() req: any) {
         return req.user;
     }
 }
