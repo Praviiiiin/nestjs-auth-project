4909d74090a8f30f3d1ad13750b46c2eb8590a65
Pravin <epravin2005@gmail.com>
2026-05-26 23:47:17 +0530

    feat: update JwtStrategy to use ConfigService for secret key management

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy (Strategy) {
-    constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(configService: ConfigService) {
         super({
             jwtFromRequest:
             ExtractJwt.fromAuthHeaderAsBearerToken(),
 
             ignoreExpiration: false,
 
-            secretOrKey: 'super-secret-key',
+            secretOrKey: configService.get<string>('JWT_SECRET')!,
         });
     }
 
