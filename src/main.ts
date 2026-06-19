6d17bc8e00a856d10098775af466df78963e9e30
Pravin <epravin2005@gmail.com>
2026-06-20 00:45:56 +0530

    feat: integrate Swagger for API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('NestJS Auth API')
+  .setDescription('Authentication and RBAC API')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    document,
+  );
 
   app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true, transform: true}));
   await app.listen(3001);
