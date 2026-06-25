7be8c2c3d3a54247212371d8a319887709809f3f
Pravin <epravin2005@gmail.com>
2026-06-25 17:29:30 +0530

    refactor: enhance email sending methods and improve logging

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,14 +1,64 @@
 import { Injectable } from "@nestjs/common";
-import { log } from "node:console";
 import * as nodemailer from 'nodemailer'
 
 @Injectable()
 export class MailService {
+    private transporter = nodemailer.createTransport({
+        service: 'gmail',
+
+        auth: {
+            user: process.env.MAIL_USER,
+
+            pass: process.env.MAIL_PASS,
+        },
+    });
+
     async sendVerificationEmail(
         email: string,
         token: string,
     ) {
-        console.log('Sending email to: ', email);
-        console.log('Verification token: ', token);;        
+        const verificationUrl = `http://localhost:3001/auth/verify-email?token=${token}`;
+        
+        await this.transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: email,
+
+            subject: 'Verify your email',
+
+            html: `<h2> Welcome to NestJS Auth </h2>
+                   <p> Click the link below to verify your account. </p>
+                   <a href = '${verificationUrl}'>
+                        Verify Email
+                    </a>
+                    `,
+        });
+
+        console.log('Verification email sent to :', email);
+    }
+
+    async sendResetPasswordEmail(
+        email: string,
+        token: string,
+    ) {
+        const resetUrl = `http://localhost:3001/auth/reset-password?token=${token}`;
+
+        await this.transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: email,
+
+            subject: 'Reset Your Password',
+
+            html: `<h2>Password Reset</h2>
+            <p>
+                Click the button below to reset your password.
+            </p>
+
+            <a href="${resetUrl}">
+                Reset Password
+            </a>
+        `,
+    });
+
+    console.log('Password reset email sent to:', email);
     }
 }
