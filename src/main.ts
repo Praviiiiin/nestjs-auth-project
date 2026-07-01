687faf02bad9daf67546118187df65a75b089f11
Pravin <epravin2005@gmail.com>
2026-07-02 00:03:31 +0530

    feat: add global HttpExceptionFilter to handle exceptions

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter())
   const config = new DocumentBuilder()
   .setTitle('NestJS Auth API')
   .setDescription('Authentication and RBAC API')
