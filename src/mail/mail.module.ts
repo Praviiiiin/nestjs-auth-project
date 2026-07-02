7df4ec779e3c94f5f2c53e296908fb27eb7174bf
Pravin <epravin2005@gmail.com>
2026-07-02 13:13:34 +0530

    feat: add MockMailService to MailModule for enhanced email handling

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
 import { MailService } from "./mail.service";
+import { MockMailService } from "./mock-mail.service";
 
 @Module({
-    providers: [MailService],
+    providers: [
+        MailService,
+        MockMailService,
+
+        {
+            provide: 'MAIL_PROVIDER',
+            useClass: MailService
+        }
+    ],
+    
     exports: [MailService],
 })
 
