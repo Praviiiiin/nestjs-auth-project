a87af7978c104342fe829b7800bcb4174ef86b3d
Pravin <epravin2005@gmail.com>
2026-07-02 12:58:23 +0530

    feat: implement MockMailService for email handling

---
+++ src/mail/mock-mail.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { MailProvider } from "./interface/mail-provider.interface";
+
+
+@Injectable()
+export class MockMailService implements MailProvider{
+    async sendVerificationEmail(
+        email: string,
+        token: string
+    ) {
+        console.log('[MOCK] Verification Email');
+        console.log(email);
+        console.log(token);
+    }
+
+    async sendResetPasswordEmail(
+        email: string, 
+        token: string
+    ) {
+        console.log(
+            '[MOCK] Reset Password'
+        );
+
+        console.log(email);
+        console.log(token);
+        
+    }            
+}
