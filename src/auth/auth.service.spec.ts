a2af9722c091264fdea38d397ea4be229922ba43
Pravin <epravin2005@gmail.com>
2026-07-06 14:09:38 +0530

    refactor: enhance AuthService test setup by adding necessary dependencies

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { MailService } from 'src/mail/mail.service';
+import { ConfigService } from '@nestjs/config';
+import { beforeEach, describe } from 'node:test';
+
 
 describe('AuthService', () => {
-  let service: AuthService;
+
+  let module: TestingModule;
+
+  let authService: AuthService;
+
+  let userService: UsersService; 
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+    module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    
+  })
+})
