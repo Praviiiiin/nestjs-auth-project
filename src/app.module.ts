1cf5ce1aa6edc80afa39be2c17b90810699ebff4
Pravin <epravin2005@gmail.com>
2026-07-07 19:27:01 +0530

    feat: configure throttler module for rate limiting in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 5,
+      }
+    ]),
+
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: 'src/.env',
@@ -15,6 +24,12 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule
   ],
+
+  providers: [{
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }]
+  
 }) 
 export class AppModule {}
 
