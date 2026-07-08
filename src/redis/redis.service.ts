b38df41f1bd023a55cc8fecd894f135ec5a409cb
Pravin <epravin2005@gmail.com>
2026-07-08 23:36:16 +0530

    feat: add logging to RedisService for connection and error handling

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { Redis } from 'ioredis'
 import { ConfigService } from '@nestjs/config'
 
@@ -6,6 +6,7 @@ import { ConfigService } from '@nestjs/config'
 @Injectable()
 export class RedisService {
 
+    private readonly logger = new Logger(RedisService.name);
     private readonly redis: Redis;
     constructor(
         private readonly configService: ConfigService
@@ -23,5 +24,13 @@ export class RedisService {
                 'REDIS_PASSWORD',
             ) || undefined,
         });
+
+        this.redis.on('connect', () => {
+            this.logger.log('Connected to Redis');
+        });
+
+        this.redis.on('error', (error) => {
+            this.logger.error(error.message)
+        })
     }
 }
