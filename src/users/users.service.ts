d24371bd56aad58aaef34acff4e98ca015b8d11d
Pravin <epravin2005@gmail.com>
2026-07-09 23:56:05 +0530

    feat: integrate Redis caching in findById method of UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,14 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema'
 import { Role } from 'src/auth/enums/role.enum';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectModel(User.name)
-        private userModel: Model<UserDocument>
+        private userModel: Model<UserDocument>,
+        private readonly redisService: RedisService,
     ) {}
-
     async create (data: Partial<User>) {
         return this.userModel.create(data);
     }
@@ -20,6 +21,13 @@ export class UsersService {
     }
 
     async findById(id: string) {
+
+        const cacheKey = `user:${id}`;
+        const cachedUser = await this.redisService.get<User>(cacheKey);
+
+        if(cachedUser) {
+            return cachedUser
+        }
         return this.userModel.findById(id);
     }
 
