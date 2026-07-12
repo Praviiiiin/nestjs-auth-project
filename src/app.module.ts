85c07b0a5d6b5df1ec9926f559c06b49e4e281f5
Pravin <epravin2005@gmail.com>
2026-07-12 23:59:35 +0530

    feat: import BullModule for queue management integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './redis/redis.module';
+import { BullModule } from '@nestjs/bullmq'
 
 @Module({
   imports: [
