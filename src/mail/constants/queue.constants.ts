c8041acb13673487418c851500e54fc78c8fa7de
Pravin <epravin2005@gmail.com>
2026-07-21 22:06:23 +0530

    feat: add QUEUE_OPTIONS constants for mail queue configuration

--- src/mail/constants/queue.constants.ts
+++ src/mail/constants/queue.constants.ts
@@ -1,3 +1,13 @@
 export const QUEUES = {
     MAIL: 'mail',
 } as const;
+
+export const QUEUE_OPTIONS = {
+    ATTEMPTS: 3,
+
+    BACKOFF_DELAY: 5000,
+
+    REMOVE_ON_COMPLETE: true,
+
+    REMOVE_ON_FAIL: false,    
+} as const 
