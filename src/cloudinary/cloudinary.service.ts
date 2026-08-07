6d07e08e62aeca933f32d9fae4bc8f975bad56f8
Pravin <epravin2005@gmail.com>
2026-08-07 18:45:27 +0530

    feat: implement CloudinaryService with image upload functionality

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { v2 as Cloudinary } from 'cloudinary'
+import { UploadApiResponse } from 'cloudinary';
+import { resolve } from 'path';
+import { Readable } from 'stream';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+
+    constructor(
+
+        @Inject('CLOUDINARY')
+        private readonly cloudinary: typeof Cloudinary
+    ) {}
+
+    async uploadImage(
+        file: Express.Multer.File,
+    ): Promise<UploadApiResponse> {
+
+        return new Promise(
+            (resolve, reject) => {
+
+                const upload = this.cloudinary.uploader.upload_stream(
+                    {
+                        folder: 'nestjs-auth',
+                    },
+                    (error, result) => {
+
+                        if (error) {
+                            return reject(error);
+                        }
+
+                        if (!result) {
+                            return reject(new Error('Upload failed: no result returned'));
+                        }
+
+                        resolve(result);
+                    },
+                );
+
+                Readable.from(
+                    file.buffer,
+                ).pipe(upload)
+            },
+        );
+    }
+}
