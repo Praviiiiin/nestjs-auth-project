acc124cb3ca9fe89e5573f30422e6b2a28a2a890
Pravin <epravin2005@gmail.com>
2026-08-09 00:06:12 +0530

    feat: implement uploadImage method in UploadService for image uploads

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+
+    constructor(
+        private readonly cloudinaryService: CloudinaryService,
+    ) {}
+
+    async uploadImage(
+        file: Express.Multer.File
+    ) {
+        const result = await this.cloudinaryService.uploadImage(
+            file,
+        );
+
+        return {
+            imageUrl: result.secure_url,
+        };
+    }
+}
