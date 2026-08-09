7f34c329d58ec4da3e228d7c7ab37a321e6a7658
Pravin <epravin2005@gmail.com>
2026-08-09 23:49:13 +0530

    feat: update uploadImage method to include userId and update avatar in UsersService

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class UploadService {
 
     constructor(
         private readonly cloudinaryService: CloudinaryService,
+        private readonly usersService: UsersService,
     ) {}
 
     async uploadImage(
-        file: Express.Multer.File
+        file: Express.Multer.File,
+        userId: string,
     ) {
         const result = await this.cloudinaryService.uploadImage(
             file,
         );
 
-        return {
-            imageUrl: result.secure_url,
-        };
+        await this.usersService.updateAvatar(
+            userId,
+            result.secure_url,
+        );
+
+        return result;
     }
 }
