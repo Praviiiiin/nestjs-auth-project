cd3ea9920422f3e3434490adfab20d10c05b4aed
Pravin <epravin2005@gmail.com>
2026-08-09 23:49:03 +0530

    feat: add UsersModule to UploadModule imports for user management

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports : [
-    CloudinaryModule
+    CloudinaryModule,
+    UsersModule
   ],
 
   controllers: [
