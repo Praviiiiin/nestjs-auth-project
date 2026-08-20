4f7148f0bfe293d64ea398633df1002e60bdd473
Pravin <epravin2005@gmail.com>
2026-08-20 23:57:38 +0530

    feat: add PasswordResetMailJob interface for password reset functionality

--- src/mail/interface/verification-mail-job.interface.ts
+++ src/mail/interface/verification-mail-job.interface.ts
@@ -2,3 +2,8 @@ export interface VerificationMailJob {
     email: string;
     verificationToken: string;
 }
+
+export interface PasswordResetMailJob {
+    email: string;
+    resetToken: string;
+}
