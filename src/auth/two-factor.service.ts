2cb3608475a4b0e7e169956d1253dfb46842fa38
Pravin <epravin2005@gmail.com>
2026-09-10 00:21:40 +0530

    feat: add methods for generating OTP authentication URL and verifying codes

--- src/auth/two-factor.service.ts
+++ src/auth/two-factor.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { generateSecret } from 'otplib';
+import { generateSecret, generateURI, verify } from 'otplib';
 
 @Injectable()
 export class TwoFactorService {
     generateSecret(): string {
         return generateSecret();
     }
+
+    generateOtpAuthUrl(
+        email: string,
+        secret: string,
+    ): string {
+        return generateURI({
+            issuer: 'NestJS Auth',
+            label: email,
+            secret,
+        });        
+    }
+
+    async verifyCode(
+        secret: string,
+        token: string,
+    ): Promise<boolean> {
+        const result = await verify({
+            secret,
+            token,
+        });
+
+        return result.valid
+    }
+    
 }
